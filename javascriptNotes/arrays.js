56df64d2938fcd78afbb5608f67b7263e2a19b13
Ali Emre Kürekci <aliemrekurekci@gmail.com>
2020-04-20 15:23:12 +0300

    array notes

---
+++ javascriptNotes/arrays.js
@@ -0,0 +1,29 @@
+var names = ["Ali", "emre", "süm","mehmet", "veli","hüs","ünz","remzi","fatih"]
+names[names.length] = "şimşek";
+names[2] = "kürekci";
+
+var io = ["keyboard", "mouse"];
+var hardware = ["CPU","Ram","SSD"];
+var computer = ["monitor","tower"];
+
+function printConsole(text){
+    console.log(text);
+}
+
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+
+var numbers = [3,45, 4, 9, 16, 25];
+
+
+function multiplyBy2(value, index, array) {
+  return value * 2;
+}
+
+
+function overThan18(value, index, array) {
+  return value > 18;
+}
+
+function summation(total, value, index, array) {
+    return total + value;
+  }
